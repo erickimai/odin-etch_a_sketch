1e2e6be6affecac622949380b6cf9511a10aba91
Erick <erick.imai.rodrigues@gmail.com>
2022-12-30 14:41:28 -0300

    Add grid

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const value = document.querySelector("#value");
-const input = document.querySelector("#grid");
-value.textContent = input.value;
+const input = document.querySelector("#gridsize");
+const grid = document.querySelector(".container-grid");
+
+value.textContent = input.value + " x " + input.value;
+createGrid(1);
 input.addEventListener("input", (event) => {
-  value.textContent = event.target.value + "x" + event.target.value;
-})
+  value.textContent = event.target.value + " x " + event.target.value;
+  createGrid(event.target.value);
+});
+
+function createGrid(value) {
+  grid.textContent = "";
+  for (let i = 1; i <= value * value; i++) {
+    let div = document.createElement("div");
+    div.setAttribute("class", "gridDiv");
+    div.style.width = 100 / value; 
+    div.style.height = 100 / value; 
+    grid.appendChild(div);
+  }
+  grid.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
+}
