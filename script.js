2b76fdda807956ee19416dc1cb3abbdbbcb212d8
Erick <erick.imai.rodrigues@gmail.com>
2022-12-31 13:53:45 -0300

    Add random and clear functionality

--- script.js
+++ script.js
@@ -2,6 +2,30 @@ const value = document.querySelector("#value");
 const input = document.querySelector("#gridsize");
 const grid = document.querySelector(".container-grid");
 const clear = document.querySelector("#clear");
+const eraser = document.querySelector("#eraser");
+const randomColor = document.querySelector("#random");
+const colorpicker = document.querySelector("#colorpicker");
+
+let color = "#000000"
+let isDrawing = false;
+let randomSelected = false;
+
+colorpicker.addEventListener("input", updateColor);
+
+eraser.addEventListener("click", () => {
+  randomSelected = false;
+  color = "rgb(255,255,255)";
+});
+
+clear.addEventListener("click", () => {
+  const allDivs = document.querySelectorAll('.gridDiv');
+  allDivs.forEach(div => div.style.backgroundColor = "white");
+  });
+
+function updateColor(e) {
+  randomSelected = false;
+  color = e.target.value;
+}
 
 value.textContent = input.value + " x " + input.value;
 createGrid(51);
@@ -11,10 +35,26 @@ input.addEventListener("input", (event) => {
 });
 
 grid.addEventListener("mousedown", (e) => {
-  e.target.style.backgroundColor = "black";
-})
+   isDrawing = true;
+   e.preventDefault();
+});
+
+grid.addEventListener("mousemove", (e) => {
+  if (randomSelected) {
+    color = "rgb(" + Math.floor(Math.random()*255) + "," + Math.floor(Math.random()*255)  + "," + Math.floor(Math.random()*255)  + ")";
+  } 
+  if (isDrawing && e.target.className === "gridDiv") {
+    e.target.style.backgroundColor = color;
+  }
+});
+
+document.body.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
 
 function createGrid(value) {
+  grid.style.backgroundColor = "white";
+  clear.click();
   grid.textContent = "";
   for (let i = 1; i <= value * value; i++) {
     let div = document.createElement("div");
@@ -24,13 +64,13 @@ function createGrid(value) {
     grid.appendChild(div);
   }
   grid.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
-  /*const allDivs = document.querySelectorAll('.gridDiv');
-  allDivs.forEach(div => {
-    div.addEventListener("mousedown", () => div.style.backgroundColor = "black")
-  });*/
 }
 
-clear.addEventListener("click", () => {
-  const allDivs = document.querySelectorAll('.gridDiv');
-  allDivs.forEach(div => div.style.backgroundColor = "white");
-  });
+randomColor.addEventListener("click", () => {
+  if (!randomSelected){
+    randomSelected = true;
+  }
+});
+
+
+
